c0a5a0161a2baf88cf2810c62c1132563c922818
fls0 <134416720+fls0@users.noreply.github.com>
2023-09-13 15:44:00 +0200

    upd

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Personal_assistant',
+    version='0.0.1',
+    description='Group-12 team project',
+    authors='PythonWizards',
+    install_requires=[
+        'dateparser==1.0.0'
+        'prompt_toolkit==3.0.21'
+        'folium==0.12.1'
+        'requests==2.26.0'
+        'pygame==2.0.1'
+        'colorama==0.4.4'
+        'dill>=0.3.7'
+        ],
+)
